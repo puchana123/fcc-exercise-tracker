f6bf72f2d793c5838a25a9d1aada239906f626ce
Puchana <puchana123@gmail.com>
2024-07-04 20:11:39 +0700

    feat: connect to database & create model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const { default: mongoose, Schema } = require('mongoose')
 require('dotenv').config()
 
 app.use(cors())
@@ -9,9 +11,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// connect to database
+mongoose.connect(process.env.MONGO_URL);
 
+// create Schema
+const userSchema = new Schema({
+  username: {type:String,unique:true},
+  count: Number,
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
+})
 
-
+// create Model
+const ExerciseUsers = mongoose.model('ExerciseUsers', userSchema);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
